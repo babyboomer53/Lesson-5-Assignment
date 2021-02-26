f2dbf2d9e5769aa2579a24369335ce02cd4db599
Edgar Cole <edgarcolejr@gmail.com>
2021-02-25 21:59:01 -0800

    I believe that the commandline processing stuff is pretty solid.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,7 @@
 
 ]
 -src/EDU/UCSD/Extension/Lesson5Concurrent.java,0\d\0defecd72584aa6c93055abe43ffb7b81e07a779
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+G
+Lesson-5-Assignment.iml,3\0\30eab4aaace146f95cb6bdbd07fe2fe8c9e9183d
--- src/EDU/UCSD/Extension/Lesson5Concurrent.java
+++ src/EDU/UCSD/Extension/Lesson5Concurrent.java
@@ -2,9 +2,41 @@ package EDU.UCSD.Extension;
 
 public class Lesson5Concurrent {
 
+    private int numberOfThreads;
+    public static long count = 0;
 
-    public void syntaxSummary() {
-        var commandName = getClass().getSimpleName();
+    private enum MODE {REENTRANT, UNLOCKED, ATOMIC}
+
+    private MODE mode = MODE.UNLOCKED;
+
+    public Lesson5Concurrent(int numberOfThreads) {
+        this.numberOfThreads = numberOfThreads;
+    }
+
+    public Lesson5Concurrent(int numberOfThreads, MODE mode) {
+        this.numberOfThreads = numberOfThreads;
+        this.mode = mode;
+    }
+
+    public static long getCount() {
+        return count;
+    }
+
+    public void setNumberOfThreads(int numberOfThreads) {
+        this.numberOfThreads = numberOfThreads;
+    }
+
+    public int getNumberOfThreads() {
+        return numberOfThreads;
+    }
+
+    public void setMode(MODE mode) {
+        this.mode = mode;
+    }
+
+
+    public static void syntaxSummary() {
+        var commandName = "Lesson5Concurrent";
         System.out.printf("%n%-7s%-18s%-20s%s%n%-7s%-18s%-20s%s%n%-7s%-18s%-20s%s%n%-7s%-18s%-20s%s%n",
                 "Usage:",
                 commandName,
@@ -24,15 +56,59 @@ public class Lesson5Concurrent {
                 "# Use locking. By default, locking is not used.");
     }
 
-    public static void main(String[] arguments) {
-        Lesson5Concurrent lesson5Concurrent = new Lesson5Concurrent();
-        String argument;
+    @Override
+    public String toString() {
+        return String.format("%nObject of: %s%nNumber of threads: %d%nMode: %s%n",
+                this.getClass().getSimpleName(),
+                this.getNumberOfThreads(),
+                this.mode);
+    }
+
+    public static void main(String[] options) {
+        String option = null;
+        MODE mode = MODE.UNLOCKED;
+        int numberOfThreads = 0;
         try {
-            argument = arguments[0];
+            option = options[0];
         } catch (IndexOutOfBoundsException indexOutOfBoundsException) {
             System.err.println("\nOops, a required option is missing!");
-            lesson5Concurrent.syntaxSummary();
+            Lesson5Concurrent.syntaxSummary();
             System.exit(1);
+        }
+        // Process the commandline options…
+        for (int index = 0; index < options.length; index++) {
+            switch (options[index]) {
+                case "--help":
+                    Lesson5Concurrent.syntaxSummary();
+                    break;
+                case "--num-threads":
+                    try {
+                        String argument = options[++index];
+                        numberOfThreads = Integer.parseInt(argument);
+                    } catch (IndexOutOfBoundsException | NumberFormatException exception) {
+                        System.err.printf("%nThe \"%s\" option requires a numeric argument.%n", options[index]);
+                        System.exit(1);
+                    }
+                    break;
+                case "--ReentrantLock":
+                    mode = MODE.REENTRANT;
+                    break;
+                case "--AtomicLong":
+                    mode = MODE.ATOMIC;
+                    break;
+                default:
+                    System.err.printf("%n\"%s\" is not a valid option!%n", options[index]);
+                    Lesson5Concurrent.syntaxSummary();
+                    System.exit(1);
+            }
+        }
+
+        if (numberOfThreads == 0) {
+            System.err.println("\nThe \"--num-threads\" option is required!");
+            Lesson5Concurrent.syntaxSummary();
+        } else {
+            Lesson5Concurrent lesson5Concurrent = new Lesson5Concurrent(numberOfThreads, mode);
+            System.out.println(lesson5Concurrent);
         }
     }
 }
